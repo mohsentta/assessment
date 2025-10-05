c473ddf255521ecc9c12eabaf8a6f7fe96ecb315
Mohsen Tarassoli <mohsentarassoli@gmail.com>
2025-10-05 23:46:40 +0330

    - turned into a car game I guess

---
+++ Assets/Scripts/Gameplay/LoadingCanvas.cs
@@ -0,0 +1,31 @@
+﻿
+
+
+
+
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LoadingCanvas : MonoBehaviour
+{
+    [SerializeField] private Canvas loadingCanvas;
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void LoadScene(int sceneNumber)
+    {
+        loadingCanvas.enabled = true;
+        SceneManager.LoadScene(sceneNumber);
+        loadingCanvas.enabled = false;
+    }
+    public void LoadScene(string sceneName)
+    {
+        loadingCanvas.enabled = true;
+        SceneManager.LoadScene(sceneName);
+        loadingCanvas.enabled = false;
+    }
+}
